b62990f6840450a446379dd0deeede0480b47531
coyote77 <w.andradec@yahoo.com>
2019-12-30 12:58:31 -0500

    Own message frontpage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mi propia pagina en AWS-React!
+          Mi propia pagina en AWS-React!!
         </p>
         <a
           className="App-link"
